edc76e650d3fea92d74b8f11c742f9ed1e221f87
Alexánder Rojas <rojjaas@gmail.com>
2018-02-11 07:41:01 -0500

    para subir

---
+++ src/main/java/com/full/face/App.java
@@ -0,0 +1,13 @@
+package com.full.face;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/full/tests/HomePageTests.java
@@ -0,0 +1,29 @@
+package com.full.tests;
+
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.PageFactory;
+import com.full.pages.HomePage;
+
+public class HomePageTests {
+	
+	WebDriver driver;
+	
+	@Test
+	public void wikipediaSearch(){
+		FirefoxOptions firefoxOptions = new FirefoxOptions();
+	    firefoxOptions.setCapability("marionette", true);
+	    driver = new FirefoxDriver(firefoxOptions);
+		PageFactory.initElements(driver, HomePage.class);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		HomePage.go(driver);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		HomePage.search("Cleopatra", "español");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.quit();
+	}
+
+}
